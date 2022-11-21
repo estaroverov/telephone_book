b73f721dfb86a5f8adeb742bcbe743e1ad14d149
eduard <you@example.com>
2022-11-21 21:10:11 +0300

    измененное дз Василия Борцова

--- export_data.py
+++ export_data.py
@@ -13,11 +13,14 @@ def export_data():
                 data.append(temp)
             elif ':' in line:
                 temp = line.strip().split(':')
-                data.append(temp)        
+                data.append(temp)
+            elif ' ' in line:
+                temp = line.strip().split(' ')
+                data.append(temp)           
             elif line != '':
                 if line != '\n':
                     t.append(line.strip())
                 else:
                     data.append(t)
-                    t= []
+                    t = []
     return data
--- import_data.py
+++ import_data.py
@@ -1,10 +1,9 @@
 # модуль импорта данных 
 
 def import_data(data, sep=None):
-    with open('phone.csv', 'a+') as file:
+    with open('phone.csv', 'a+',encoding='UTF-8') as file:
         if sep == None:
-            for i in data:
-                file.write(f"{i}\n")
+            file.write(','.join(data))
             file.write(f"\n")
         else:
             file.write(sep.join(data))
